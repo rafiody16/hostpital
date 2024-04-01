ea0c952f84ff4853cdf609899e3752488381f926
RAFI ODY PRASETYO <rafiody16@gmail.com>
2024-04-01 12:24:23 +0700

    add api

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\PasienController;
 use App\Http\Controllers\DokterController;
+use App\Http\Controllers\AppointmentController;
+use App\Http\Controllers\MedicalRecordController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -27,4 +29,16 @@ Route::put('dokter/{id}', [DokterController::class, 'update']);
 Route::delete('dokter/{id}', [DokterController::class, 'delete']);
 Route::get('dokter', [DokterController::class, 'getAll']);
 Route::get('dokter/{id_dokter}', [DokterController::class, 'getById']);
+
+Route::post('appointment', [AppointmentController::class, 'insert']);
+Route::put('appointment/{id}', [AppointmentController::class, 'update']);
+Route::delete('appointment/{id}', [AppointmentController::class, 'delete']);
+Route::get('appointment', [AppointmentController::class, 'getAll']);
+Route::get('appointment/{id_appointment}', [AppointmentController::class, 'getById']);
+
+Route::post('medicalrecord', [MedicalRecordController::class, 'insert']);
+Route::put('medicalrecord/{id}', [MedicalRecordController::class, 'update']);
+Route::delete('medicalrecord/{id}', [MedicalRecordController::class, 'delete']);
+Route::get('medicalrecord', [MedicalRecordController::class, 'getAll']);
+Route::get('medicalrecord/{id_medical_record}', [MedicalRecordController::class, 'getById']);
 
